3fd502457b97456491470e671076b00606beda73
Joe V <joeveverka89@gmail.com>
2020-08-24 11:55:27 -0400

    Added Xcode Project.

---
+++ Cellular Automatom/Cellular Automatom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cellular Automatom
+//
+//  Created by Joseph Veverka on 8/24/20.
+//  Copyright © 2020 Joseph Veverka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
