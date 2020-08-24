eb5a33d8892b4cd5afbab37d6f9c543987452ff3
Joe V <joeveverka89@gmail.com>
2020-08-24 12:42:32 -0400

    Added some view controllers.

--- Cellular Automatom/Cellular Automatom/Models/Views/CellView.swift
+++ Cellular Automatom/Cellular Automatom/Models/Views/CellView.swift
@@ -13,4 +13,35 @@ class CellView: UIView {
     //MARK: - Properties
     
     var isAlive: Bool = false
+    
+    //MARK: Class Functions
+    
+    func configureCell() {
+        
+        self.backgroundColor = UIColor(white: 10, alpha: 0.5)
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1.5
+        
+    }
+    
+    func returnCoords() -> (x: Int, y: Int)? {
+        
+        let x = 1
+        let y = 2
+        let z = (x, y)
+        
+        return z
+    }
+    
+    func murderCell() {
+        
+    }
+    
+    func birthACell() {
+        
+    }
+    
+     
+    
+    
 }
