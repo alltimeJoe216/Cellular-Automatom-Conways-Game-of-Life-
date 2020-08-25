7be827db7b321544436f1227c2086bc4ad378f27
Joe V <joeveverka89@gmail.com>
2020-08-25 19:13:57 -0400

    Storyboard and VC work

--- Cellular Automatom/Cellular Automatom/Models/Views/PresetCVCell.swift
+++ Cellular Automatom/Cellular Automatom/Models/Views/PresetCVCell.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2020 Joseph Veverka. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PresetCell: UITableViewCell {
+    
+    static var identifier: String = "PresetCell"
+    
+    var preset: ShapePreset!
+    var label = UILabel()
+    
+    func set(preset: ShapePreset ) {
+
+        translatesAutoresizingMaskIntoConstraints = false
+        self.preset = preset
+        label.text = preset.currentPreset.rawValue.capitalized
+        label.translatesAutoresizingMaskIntoConstraints = false
+        preset.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(label)
+        
+        
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            label.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
+    
+    
+    override func layoutSubviews() {
+
+        contentView.layer.cornerRadius = 10
+        contentView.layer.borderWidth = 0.5
+        contentView.layer.backgroundColor = UIColor.white.cgColor
+        contentView.backgroundColor = UIColor(white: 1, alpha: 0.40)
+        layer.backgroundColor = UIColor.clear.cgColor
+    }
+
+    
+}
--- Cellular Automatom/Cellular Automatom/ViewControllers/GameViewController.swift
+++ Cellular Automatom/Cellular Automatom/ViewControllers/GameViewController.swift
@@ -25,39 +25,153 @@ class GameViewController: UIViewController {
     //MARK: - Class Properties
     var gridView: GridView!
     var genSpeedTimer = Timer()
+    var gameIsRunning = false
+    let presetView = UIView()
+    var presetTableView = UITableView()
+    var label = UILabel()
      
 
     //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.gridView = GridView(gridWidth: self.view.frame.width, gridHeight: self.view.frame.height, gridView: self.view)
         // Do any additional setup after loading the view.
     }
     
-
+    //MARK: - IBActions
+    
+    @IBAction func resetButton(_ sender: Any) {
+    }
+    
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+    }
+    
+    //MARK: - Grid and Cell Related Functions
+    
+    func configureCurrentPresetLabel() {
+        label.text = "Current Brush: Dot"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: resetButton.bottomAnchor, constant: 5),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+        ])
+
+    }
+//
+//    func configureCurrentPresetView(index: Int) {
+//        if grid.currentPreset != nil { grid.currentPreset.removeFromSuperview() }
+//        let selectedPreset = grid.presets[index]
+//        let preset = ShapePreset(size: selectedPreset.size, cellWidth: selectedPreset.cellWidth, brushType: selectedPreset.currentBrush)
+//        grid.currentPreset = preset
+//        preset.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(preset)
+//
+//        NSLayoutConstraint.activate([
+//            preset.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 5),
+//            preset.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -preset.frame.width / 2)
+//        ])
+//
+//        label.text = "Current Brush: " + grid.currentPreset.currentBrush.rawValue.capitalized
+//    }
+    
+    
+    
+//    func configurePresetBar() {
+//        presetView.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(presetView)
+//        presetView.backgroundColor = .clear
+//        NSLayoutConstraint.activate([
+//            presetView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+//            presetView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            presetView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            presetView.topAnchor.constraint(equalTo: grid.screenArray[24][24].bottomAnchor)
+//        ])
+//    }
+    
+//    func setupPreset() {
+//        let tableView = UITableView(frame: .zero, style: .plain)
+//        tableView.translatesAutoresizingMaskIntoConstraints = false
+//        self.view.addSubview(tableView)
+//        NSLayoutConstraint.activate([
+//            self.presetView.topAnchor.constraint(equalTo: tableView.topAnchor),
+//            self.presetView.bottomAnchor.constraint(equalTo: tableView.bottomAnchor),
+//            self.presetView.leadingAnchor.constraint(equalTo: tableView.leadingAnchor),
+//            self.presetView.trailingAnchor.constraint(equalTo: tableView.trailingAnchor),
+//        ])
+//        self.presetTableView = tableView
+//    }
+    
+//    func setupTableView() {
+//        self.presetTableView.dataSource = self
+//        self.presetTableView.delegate = self
+//        self.presetTableView.allowsSelection = true
+//        self.presetTableView.register(PresetCell.self, forCellReuseIdentifier: "PresetCell")
+//        presetTableView.backgroundColor = .clear
+//    }
+//
+    
+    
+    func configurePresets() {
+        gridView.presets.append(ShapePreset(size: 1, cellWidth: gridView.cellDims, presetStyle: .singleCell))
+        gridView.presets.append(ShapePreset(size: 2, cellWidth: gridView.cellDims, presetStyle:  .block))
+        gridView.presets.append(ShapePreset(size: 3, cellWidth: gridView.cellDims, presetStyle:  .glider))
+        gridView.presets.append(ShapePreset(size: 1, cellWidth: gridView.cellDims, presetStyle: .random))
     }
 
 }
+
+
 /*/
  
  This is going to house a small collection view of grid presets, maybe with a small photo showing what each one looks like
  
  */
-extension GameViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+extension GameViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return gridView.presets.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PresetCell", for: indexPath) as! PresetCell
+        
+        cell.set(preset: gridView.presets[indexPath.row])
+        return cell
     }
     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        configureCurrentPresetView(index: indexPath.row)
+
+        
+        if indexPath.row == 6 {
+            
+        gridView.resetGrid(grid: gridView.bottomGrid)
+            
+            for x in 0...49 {
+                
+                for y in 0...49 {
+                    
+                    let randomInt = Int.random(in: 0...4)
+                    if randomInt == 1 {
+                        gridView.bottomGrid[x][y].birthACell()
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
     
 }
+
