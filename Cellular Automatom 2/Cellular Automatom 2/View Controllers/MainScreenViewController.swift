40ce37183c71674505cf00091b76509086657023
Joe V <joeveverka89@gmail.com>
2020-08-26 19:09:25 -0400

    UI Changes. Code clean up.

--- Cellular Automatom 2/Cellular Automatom 2/View Controllers/MainScreenViewController.swift
+++ Cellular Automatom 2/Cellular Automatom 2/View Controllers/MainScreenViewController.swift
@@ -25,6 +25,9 @@ class MainScreenViewController: UIViewController {
     //MARK: - View Lifecycle ##
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default) 
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
         
 
         setupCollectionView()
